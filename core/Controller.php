a96e34891bed33aead2bc70edfb167f5c62411af
Jamie Long <jamielong1221@gmail.com>
2020-08-15 22:38:42 +0100

    Enabled handling of data with Controllers

--- core/Controller.php
+++ core/Controller.php
@@ -9,8 +9,14 @@ namespace app\core;
 
  class Controller
  {
-     public function render($view, $params = [])
-     {
-        return Application::$app->router->renderView($view, $params);
-     }
+    public string $layout = 'main';
+    public function setLayout($layout)
+    {
+        $this->layout = $layout;
+    }
+
+    public function render($view, $params = [])
+    {
+       return Application::$app->router->renderView($view, $params);
+    }
  }
