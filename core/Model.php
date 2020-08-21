d011b7f71a7f548ecfb74cb34a67d4754b0a5468
Jamie Long <jamielong1221@gmail.com>
2020-08-21 23:44:34 +0100

    Improving validation mapping

--- core/Model.php
+++ core/Model.php
@@ -1,6 +1,6 @@
 <?php
 
-// https://www.youtube.com/watch?v=ZSYhQkM5VIM&t=235s @22:00
+// https://www.youtube.com/watch?v=ZSYhQkM5VIM&t=235s @47:10
 
 namespace app\core;
 
@@ -42,15 +42,30 @@ abstract class Model
                 if ($ruleName === self::RULE_REQUIRED && !$value) {
                     $this->addError($attribute, self::RULE_REQUIRED);
                 }
+                if ($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+                if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                }
             }
         }
 
         return empty($this->errors);
     }
 
-    public function addError(string $attribute, string $rule)
+    public function addError(string $attribute, string $rule, $params = [])
     {
         $message = $this->errorMessages()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
         $this->errors[$attribute][] = $message;
     }
 
@@ -65,4 +80,13 @@ abstract class Model
         ];
     }
 
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError($attribute)
+    {
+        return $this->errors[$attribute][0] ?? false;
+    }
 }
--- views/register.php
+++ views/register.php
@@ -1,11 +1,23 @@
 <h1>Create an account</h1>
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+  <?php echo $form->field($model, 'firstname') ?>
+  <?php echo $form->field($model, 'lastname') ?>
+  <?php echo $form->field($model, 'email') ?>
+  <?php echo $form->field($model, 'password') ?>
+  <?php echo $form->field($model, 'confirmPassword') ?>
 
+  <button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\FORM::end() ?>
 <form action = "" method = "post">
   <div class="row">
     <div class="col">
       <div class="form-group">
         <label>First Name</label>
-        <input type="text" name="firstname" class="form-control">
+        <input type="text" name="firstname" 
+        class="form-control<?php echo $model->hasError('firstname') ? ' is-invalid' : ''?>">
+        <div class="invalid-feedback">
+          <?php echo $model->getFirstError('firstname') ?>
+        </div>
       </div>
     </div>
     <div class="col">
