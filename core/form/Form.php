a7ac5d1cea0412ba87fd72f0a5673cfd8b264678
Jamie Long <jamielong1221@gmail.com>
2020-08-23 00:28:16 +0100

    finish form validation

--- core/form/Form.php
+++ core/form/Form.php
@@ -12,7 +12,7 @@ class Form
 {
     public static function begin($action, $method)
     {
-        echo sprintf('<form action="%s method="%s">', $action, $method);
+        echo sprintf('<form action="%s" method="%s">', $action, $method);
         return new Form();
     }
 
--- views/register.php
+++ views/register.php
@@ -1,44 +1,16 @@
 <h1>Create an account</h1>
 <?php $form = \app\core\form\Form::begin('', "post") ?>
-  <?php echo $form->field($model, 'firstname') ?>
-  <?php echo $form->field($model, 'lastname') ?>
+  <div class="row">
+    <div class="col">
+      <?php echo $form->field($model, 'firstname') ?>
+    </div>
+    <div class="col">
+      <?php echo $form->field($model, 'lastname') ?>
+    </div>
+  </div>
   <?php echo $form->field($model, 'email') ?>
-  <?php echo $form->field($model, 'password') ?>
-  <?php echo $form->field($model, 'confirmPassword') ?>
+  <?php echo $form->field($model, 'password')->passwordField() ?>
+  <?php echo $form->field($model, 'confirmPassword')->passwordfield() ?>
 
   <button type="submit" class="btn btn-primary">Submit</button>
 <?php \app\core\form\FORM::end() ?>
-<form action = "" method = "post">
-  <div class="row">
-    <div class="col">
-      <div class="form-group">
-        <label>First Name</label>
-        <input type="text" name="firstname" 
-        class="form-control<?php echo $model->hasError('firstname') ? ' is-invalid' : ''?>">
-        <div class="invalid-feedback">
-          <?php echo $model->getFirstError('firstname') ?>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="form-group">
-        <label>Last Name</label>
-        <input type="text" name="lastname" class="form-control">
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <label>Email</label>
-    <input type="text" name="email" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Password</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Confirm Password</label>
-    <input type="password" name="confirmPassword" class="form-control">
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
